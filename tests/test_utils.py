c894bf9754a44cb4a0de26bdc95da65db0d08401
Katsuya SAITO <hello@skatsuya.com>
2017-11-16 21:26:08 +0900

    Add: test for create_task

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
-from chatwork.utils import send_chatwork
+from chatwork.utils import send_chatwork, create_task
 
 
 class SendChatworkTests(TestCase):
     def test_send_chatwork(self):
         response = send_chatwork('test', 1234)
         self.assertEqual(response, {'message_id': '1234'})
+
+
+class CreateTaskTests(TestCase):
+    def test_create_task(self):
+        response = create_task('test', 1234, [123, 124])
+        self.assertEqual(response, {'task_ids': [123, 124]})
